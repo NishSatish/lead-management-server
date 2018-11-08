c9a55b5ea8991f5075c7fa05d10642f0b14bc4ff
nishsatish <nishantsatishkumar@gmail.com>
2018-11-08 22:16:30 +0530

    Bug fixed
    Sample mocha test case setup.
    
    Squahed all commits

--- public/js/config.js
+++ public/js/config.js
@@ -1,9 +1,11 @@
-const config = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-};
-firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyCR2QwBBw137kuZed0wt96HniEiH5VZOtA",
+    authDomain: "gcitest-1804.firebaseapp.com",
+    databaseURL: "https://gcitest-1804.firebaseio.com",
+    projectId: "gcitest-1804",
+    storageBucket: "",
+    messagingSenderId: "1081590970299"
+  };
+  firebase.initializeApp(config);
 
 const CLIENT_ID = null;
---
+++ test/case.test.js
@@ -0,0 +1,7 @@
+const expect = require("chai").expect;
+
+describe('Sample test case', () => {
+    it("Muss equal sein", () => {
+        expect(5).to.equal(5);
+    });
+});
